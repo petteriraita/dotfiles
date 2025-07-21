7162da3aea285e5ae49085c638db4862e8e2480e
Petteri Raita <petteri.r.raita@gmail.com>
2025-07-21 18:08:16 +0300

    add a checker to run the overview of the project

--- bin/dualcheck.sh
+++ bin/dualcheck.sh
@@ -1,14 +1,25 @@
-list=("/home/petteri/repos/github/dual-function-keys/mappings.yaml" "/etc/interception/udevmon.d/my-udevmon.yaml")
-
+list=("/home/petteri/repos/github/dual-function-keys/my-mappings.yaml" "/etc/interception/udevmon.d/my-udevmon.yaml")
+commandlist=(
+    "sudo systemctl status udevmon" 
+    "ls -l $HOME/repos/github/dual-function-keys/" 
+    "ls -l /etc/interception" 
+    "xinput list"
+)
 
 # the function reads the file list, cats them, then copies to the clipboard
 myfunc() {
 for file in "${list[@]}"; do
-    echo -e "\nReading "$file" ===================\n"
     echo -e "\nReading $file ===================\n"
     cat "$file"
 
 done
+echo -e "\n"
+for cmd in "${commandlist[@]}"; do
+    echo "$cmd"
+    # prefixing the command with a space to make sure that the command doesnt mix '-' characters
+    eval " $cmd"
+done
 }
+
 
 myfunc | tee >(xclip -selection clipboard)
