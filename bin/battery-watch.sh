cff0acc823d00cd0b30807d82613386f9a5b14a8
petteri raita <petteri.r.raita@gmail.com>
2026-02-08 14:20:18 +0100

    nvim, kanata, battery service

--- bin/battery-watch.sh
+++ bin/battery-watch.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
 
-notify-send "test"
-capacity=$(cat /sys/class/power_supply/BAT0/capacity)
-status=$(cat /sys/class/power_supply/BAT0/status)
-
-echo "${capacity}"
-echo "${status}"
-
+about_to_suspend=1
 while true; do
+	## get the capacities
+	capacity=$(cat /sys/class/power_supply/BAT0/capacity)
+	status=$(cat /sys/class/power_supply/BAT0/status)
+
+	### the basic reset if everything is okay or charging started
+	if [[ "$capacity" -ge 30 || "$status" == "Charging" ]]; then
+		about_to_suspend=1
+	fi
+
 	if [[ "$status" == "Discharging" && "$capacity" -le 25 ]]; then
 		notify-send "Low Battery" "Battery at ${capacity}%"
 	fi
@@ -16,8 +19,15 @@ while true; do
 		notify-send -u critical "CRITICAL BATTERY" "Plug in charger!"
 	fi
 
-	if [[ "$status" == "Discharging" || "$status" == "Not charging" && "$capacity" -le 15 ]]; then
-		systemctl suspend
+	if [[ ("$status" == "Discharging" || "$status" == "Not charging") && "$capacity" -le 15 ]]; then
+		### so if the about_to_suspend was set already to 0 the last time, then we just suspend
+		if [[ "$about_to_suspend" -eq 0 ]]; then
+			about_to_suspend=1
+			systemctl suspend
+		fi
+		### if its the first, time, giving the user 120 seconds
+		notify-send -u critical "suspending in 120seconds"
+		about_to_suspend=0
 	fi
-	sleep 60
+	sleep 120
 done
--- nvim/init.vim
+++
@@ -1,5 +0,0 @@
-set clipboard=unnamedplus
-set number
-set relativenumber
-set expandtab shiftwidth=4 softtabstop=4 tabstop=4
-
