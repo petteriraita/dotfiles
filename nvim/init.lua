15e9a50603b42258423c2dab22ed4595a5105313
petteri raita <petteri.r.raita@gmail.com>
2026-04-03 09:17:15 +0200

    snippet

--- nvim/init.lua
+++ nvim/init.lua
@@ -322,6 +322,32 @@ require('lazy').setup({
       require('nvim-autopairs').setup {}
     end,
   },
+  -- petteri start of the java plugin
+  {
+    'mfussenegger/nvim-jdtls',
+    config = function()
+      local jdtls = require 'jdtls'
+
+      local root_markers = { '.git', 'mvnw', 'gradlew', 'pom.xml', 'build.gradle' }
+
+      vim.api.nvim_create_autocmd('FileType', {
+        pattern = 'java',
+        callback = function()
+          local root_dir = require('jdtls.setup').find_root(root_markers)
+          if root_dir == nil then
+            return
+          end
+
+          local config = {
+            cmd = { vim.fn.stdpath 'data' .. '/mason/bin/jdtls' },
+            root_dir = root_dir,
+          }
+
+          jdtls.start_or_attach(config)
+        end,
+      })
+    end,
+  },
   -- Petteri --- start of my second plugin (vimtex)
   {
     'lervag/vimtex',
